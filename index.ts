8baaa48ab9baeec46ae4a8e719844b1e70ac2eba
ArfaShoaib <arfa67264@gmail.com>
2024-03-23 08:29:41 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const Answers = await inquirer.prompt([
+  {
+    message: "Enter first number",
+    type: "number",
+    name: "Firstnum",
+  },
+  {
+    message: "Enter second number",
+    type: "number",
+    name: "Secondnum",
+  },
+  {
+    message: "choose operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Substract", "Multiply", "Division"],
+  },
+]);
+
+// Conditional Statement
+
+if (Answers.operator === "Addition") {
+  console.log(Answers.Firstnum + Answers.Secondnum);
+} else if (Answers.operator === "Substract") {
+  console.log(Answers.Firstnum - Answers.Secondnum);
+} else if (Answers.operator === "Multiply") {
+  console.log(Answers.Firstnum * Answers.Secondnum);
+} else if (Answers.operator === "Division") {
+  console.log(Answers.Firstnum / Answers.Secondnum);
+} else {
+  console.log("Please Select Valid Operator");
+}
